e446cf8432cc0ce9aaa9507e081e74d3b1b4edc5
hakan <hakan@hakan>
2026-03-29 16:32:09 +0300

    start 2026

--- send_contact.php
+++ send_contact.php
@@ -1,9 +1,12 @@
 <?php
+session_start();
+
 include_once('assets/class.phpmailer.php');
 include_once('assets/class.smtp.php');
-require_once 'assets/php-spam-filter/spamfilter.php';
+include_once('include/php-spam-filter/spamfilter.php');
+include_once('include/black_ip.php');
+
 $filter = new SpamFilter();
-session_start();
 
 if ($_SESSION['revalid'] != "revalid") {
 
@@ -45,23 +48,25 @@ $subject     = $_POST['subject'];
 $message     = $_POST['message'];
 $phone       = $_POST['phone'];
 
+$result1 = $filter->check_text($name); if ($result1) { echo "nok"; exit; }
+$result2 = $filter->check_text($email); if ($result2) { echo "nok"; exit; }
+$result3 = $filter->check_text($_phone); if ($result3) { echo "nok"; exit; }
+$result4 = $filter->check_text($subject); if ($result4) { echo "nok"; exit; }
+$result5 = $filter->check_text($message); if ($result5) { echo "nok"; exit; }
 
-$result1 = $filter->check_text($name);
-if ($result1) { echo "nok"; exit; }
-$result2 = $filter->check_text($email);
-if ($result2) { echo "nok"; exit; }
-$result3 = $filter->check_text($subject);
-if ($result3) { echo "nok"; exit; }
-$result4 = $filter->check_text($message);
-if ($result4) { echo "nok"; exit; }
-$result5 = $filter->check_text($phone);
-if ($result5) { echo "nok"; exit; }
+if (contains($spam_list, $name) == TRUE) { echo "bad"; exit; }
+if (contains($spam_list, $email) == TRUE) { echo "bad"; exit; }
+if (contains($spam_list, $_phone) == TRUE) { echo "bad"; exit; }
+if (contains($spam_list, $subject) == TRUE) { echo "bad"; exit; }
+if (contains($spam_list, $message) == TRUE) { echo "bad"; exit; }
 
-if (empty($name) OR strlen($name) < 9) { echo "nok"; exit; }
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { echo "nok"; exit; }
-if (empty($phone) OR strlen($phone) < 10) { echo "nok"; exit; }
-if (empty($subject)) { echo "nok"; exit; }
-if (strlen($message) < 15) { echo "nok"; exit; }
+if (empty($name)) { echo "name"; exit; }
+if (empty($_phone) OR strlen($_phone) < 10) { echo "phone"; exit; }
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { echo "emailbad"; exit; }
+if (empty($email) OR strlen($email) < 9) { echo "email"; exit; }
+if (empty($subject)) { echo "message"; exit; }
+if (empty($message)) { echo "message"; exit; }
+if (strlen($message) < 15) { echo "message_short"; exit; }
 
 $email_title = date("d-m-Y h:i")." - Hakan ERENLER";
 
@@ -81,11 +86,11 @@ $mail->SMTPOptions = array(
                     'allow_self_signed' => true
                     )
                     );
-$mail->SMTPSecure  = "SSL";
+$mail->SMTPSecure  = "TSL";
 $mail->Host        = "smtppro.zoho.eu";
 $mail->Port        = 587;
 $mail->Username    = "info@hakanerenler.net";
-$mail->Password    = "wbFAQeTLCR2i";
+$mail->Password    = "wbFA QeTL CR2i";
 $mail->IsHTML(true);
 $mail->SetFrom("info@hakanerenler.net", "Hakan Erenler Web Development");
 $mail->Subject     = $subject;
